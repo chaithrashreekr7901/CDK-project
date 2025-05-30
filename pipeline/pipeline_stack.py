7bc565bb85d6e0abc16ae314f2ac5d9b055517c3
chaithrashreekr7901 <145109539+chaithrashreekr7901@users.noreply.github.com>
2025-05-30 22:08:27 +0530

    pieline updated based on the role

--- pipeline/pipeline_stack.py
+++ pipeline/pipeline_stack.py
@@ -36,12 +36,7 @@ class PipelineStack(Stack):
             auto_delete_objects=True,
             versioned=True,
             encryption=s3.BucketEncryption.S3_MANAGED,
-            block_public_access=s3.BlockPublicAccess(
-            block_public_acls=False,
-            block_public_policy=False,
-            ignore_public_acls=False,
-            restrict_public_buckets=False,
-            ),  # Allow public access
+            block_public_access=s3.BlockPublicAccess.NONE,  # Allow public access
             public_read_access=True,
         )
 
@@ -53,12 +48,7 @@ class PipelineStack(Stack):
             auto_delete_objects=True,
             versioned=True,
             encryption=s3.BucketEncryption.S3_MANAGED,
-            block_public_access=s3.BlockPublicAccess(
-            block_public_acls=False,
-            block_public_policy=False,
-            ignore_public_acls=False,
-            restrict_public_buckets=False,
-            ),  
+            block_public_access=s3.BlockPublicAccess.NONE,  # Allow public access
             public_read_access=True,
         )
 
