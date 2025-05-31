e9f77182b1e2b277618e617eadc4952d247a0d9e
chaithrashreekr7901 <145109539+chaithrashreekr7901@users.noreply.github.com>
2025-05-31 23:44:32 +0530

    pipe;ine

--- pipeline/pipeline_stack.py
+++ pipeline/pipeline_stack.py
@@ -118,7 +118,7 @@ class PipelineStack(Stack):
                         codepipeline_actions.CloudFormationCreateUpdateStackAction(
                             action_name="Deploy_CF_Template",
                             stack_name=cdk_infra_stack_name,
-                            template_path=cdk_output.at_path(f"MyMainInfrastructureStack.template.json"),
+                            template_path=cdk_output.at_path("MyMainInfrastructureStack.template.json"),
                             admin_permissions=True,
                         )
                     ],
